c7e2e8d3bcad9e0e65653f4d4460d0efcb83ab4e
najmul-hassan <najmulhassan032@gmail.com>
2026-04-18 20:57:15 +0600

    fix ssl for aiven mysql

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -4,19 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST, // ❌ localhost remove
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-
-  // 🔥 IMPORTANT for Aiven
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
 });
 
